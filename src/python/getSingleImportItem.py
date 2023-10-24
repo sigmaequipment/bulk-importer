5feb9583d57e223c620a4a7a9442f132609d45be
bweisman-sigma <bweisman@sigmaequipment.com>
2023-10-24 16:32:39 -0500

    Get Items for Bulk and Single Import Scripts

---
+++ src/python/getSingleImportItem.py
@@ -0,0 +1,135 @@
+import json
+from datetime import date
+import requests
+import threading
+import time
+from importerFunctions import uploadFailedImports
+
+
+#connection string for the PostgREST API to access the table needed to import a new item
+PostgREST_Table_String = "http://10.100.100.42:8390/approved_templates"
+
+#Connection string to the JS importer
+Importer_URL = "http://10.100.100.51:3005/import"
+
+#load the user and tenant token json
+with open('src/json/SkuVaultTenantandUserTokens.json','r+') as tenant:
+    tenanttoken = json.load(tenant)
+
+#load the CA API json
+with open('src/json/ChannelAdvisorAPI.json','r+') as channel:
+    datachannel = json.load(channel)
+
+#load the SV API json
+with open('src/json/SkuVaultAPI.json', 'r+') as skuvaultjson:
+    dataskuvault = json.load(skuvaultjson)
+
+#load the last sku that was imported by the single import script
+with open('src/json/singleSKU.json', 'r+') as skuFile:
+    lastImport = json.load(skuFile)
+
+
+#modify the file that holds the sku of the last item that was imported via the single importer
+def updateSingleSKUFile(sku):
+    lastImport["singleSKU"] = sku
+    with open('src/json/singleSKU.json', 'w') as currentSKU:
+        json.dump(lastImport, currentSKU)
+
+
+#Background task to collect non user created items to be sent to the single importer
+class BackgroundTaskSingleImport(threading.Thread):
+    def getItemsForImport():
+        while True:
+
+            #list to store items that will be part of payload
+            items = []
+            
+            #sku of item to be imported
+            sku = 0
+            
+            #sku that was imported last
+            sku_of_last_import = lastImport["singleSKU"]
+
+            #query to get the items that need to be imported
+            single_import_query = requests.get(f'{PostgREST_Table_String}?select=*&and=(inventory_sku.gt.{sku_of_last_import},or(source.eq."MANUAL CREATION", source.eq."SERIES GENERATOR"))&order=inventory_sku.asc&limit=1').json()
+
+            #if the queury returns an empty list wait 30s and then look again for more items
+            if single_import_query == []:
+                print("Waiting 30 seconds")
+                time.sleep(30)
+                continue
+
+            #get the item from the query
+            item = single_import_query[0]
+
+            print(f"Importing {item['inventory_sku']}")
+
+            #convert data from query to dictionary
+            item_to_dictionary = {
+                "inventory_sku" : item["inventory_sku"],
+                "item_name" : item["item_name"],
+                "lister_sku" : item["lister_sku"],
+                "part_number" : item["part_number"],
+                "brand" : item["brand"],
+                "description" : item["description"],
+                "suggested_category" : item["suggested_category"],
+                "sigma_category" : item["sigma_category"],
+                "weight" : item["weight"],
+                "link" : item["link"],
+                "apn" : item["apn"],
+                "series" : item["series"],
+                "sigma_old_sku" : item["sigma_old_sku"],
+                "sigma_old_part_number" : item["sigma_old_part_number"],
+                "estimated_value" : item["estimated_value"],
+                "authorized_distr_brokerage_price" : item["authorized_distr_brokerage_price"],
+                "original_packaging_price" : item["original_packaging_price"],
+                "radwell_packaging_price" : item["radwell_packaging_price"],
+                "refurbished_price" : item["refurbished_price"],
+                "repair_price" : item["repair_price"],
+                "last_price_update" : item["last_price_update"],
+                "scrape_time" : item["scrape_time"],
+                "approval_time" : item["approval_time"],
+                "user_who_approved" : item["user_who_approved"],
+                "source" : item["source"],
+            }
+
+            #append item to list
+            items.append(item_to_dictionary)
+
+            #save sku of item to be saved later
+            sku = item["inventory_sku"]
+
+            #set up dictionary for tokens
+            tokens = {
+                "clientid": datachannel['clientid'],
+                "clientsecret": datachannel['clientsecret'],
+                "refreshtoken": datachannel['refreshtoken'],
+                "TenantToken": tenanttoken[item["user_who_approved"]]["TenantToken"],
+                "UserToken": tenanttoken[item["user_who_approved"]]["UserToken"]
+            }
+
+            #set up payload to be sent to API 
+            payload = json.dumps({"items" : items, "tokens" : tokens}, indent=4, default=str)
+            
+            #TODO set up request to Michael's endpoint and handle response
+            headers = {}
+            importer_request = requests.post(url=Importer_URL, headers=headers, data=payload).json()
+            
+            print(importer_request)
+
+            #get list of skus that returned errors
+            #TODO check with Michael on recieving errors
+            importer_request_errors =  importer_request["badSkus"]
+
+            #upload the list of failed skus to the reimport table
+            if (importer_request_errors != []):
+                uploadFailedImports(importer_request_errors)
+
+            #update the json storing the last imported sku
+            updateSingleSKUFile(sku)
+
+
+#What runs when the script is directly called
+if __name__ == "__main__":
+    obj = BackgroundTaskSingleImport()
+    obj.getItemsForImport()
---
+++ src/python/importerFunctions.py
@@ -0,0 +1,41 @@
+import requests
+import json
+
+#connection string for the PostgREST API to access the table needed to import a new item
+PostgREST_Table_String = "http://10.100.100.42:8390/approved_templates"
+
+#function to hanlde items that result in a failed import
+def uploadFailedImports(failed_skus):
+    #init fields for requests to the reimport table
+    reimport_table = "http://10.100.100.42:8390/reimport_needed"
+    headers = {
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic3VycGx1c191c2VyIn0.mbykDI_CBEDdxyTxqGECh7TwtrRpJPAznPrE291dtww',
+        "Content-Type": "application/json"
+        }
+
+    #loop through all skus that returned an error, get data on those sku's from the approved table, and add those skus and respective data with that sku to the reimport table
+    #TODO add cases for different errors
+    for failure in failed_skus:
+        current_sku = int(failure["Sku"])
+
+        print(f'{current_sku} being reimported')
+
+        sku_query = requests.get(f'{PostgREST_Table_String}?select=item_name,series,source&inventory_sku=eq.{current_sku}').json()
+
+        #TODO handle no response from approved_table
+        if (sku_query == []):
+            continue
+
+        #init item for reimport table
+        current_item = sku_query[0]
+        payload = json.dumps({
+            "inventory_sku": current_sku,
+            "item_name": current_item["item_name"],
+            "series": current_item["series"],
+            "source": current_item["source"]
+        }, indent=4, default=str)
+
+        #post to API
+        reimport_response = requests.post(url=reimport_table, headers=headers, data=payload)
+
+        print(reimport_response)
