b90b31b2f2257ab012127ed5ab7e68d243cb353e
mwalkersigma <131814372+mwalkersigma@users.noreply.github.com>
2023-10-26 10:15:02 -0500

    Cleaning up logging.

--- main.js
+++ main.js
@@ -9,6 +9,15 @@ const SkuVaultImporter = require("./src/javascript/skuVault/importer");
 const channelAdvisorImport = require("./src/javascript/ChannelAdvisor/fullImport");
 const {log,error} = require("./src/javascript/Logger/logger");
 const splitPayload = require('./src/javascript/splitPayload/splitPayload');
+const seperator = "------------------------------------"
+log(seperator)
+log("*** Bulk Importer (Beta) V1 ***");
+log("*** Last Updated: 10/26/23 ***");
+log("Original Python Code By: Christian Guzman & Brandon Weisman");
+log("Current Javascript Iteration By: Michael Walker");
+log("Proud to be employee owned");
+log(seperator)
+
 
 const fastify = Fastify({
     logger: true
@@ -37,11 +46,16 @@ fastify.post('/import',incomingPayloadSchema, async (request,reply) => {
     const {body:{items,tokens}} = request;
     let length = items.length;
     console.log(JSON.stringify(items,null,2))
+    log(seperator)
     log(`Request To Start Import of ${length} Items`);
     const badSkus = [];
     const completedItems = [];
     let {channelAdvisorPayload,skuVaultPayload} = await createPayloads(items);
     log(`Finished Creating Payloads`)
+    log(`Sku Vault Payload Length: ${skuVaultPayload.length}`)
+    log(`Channel Advisor Payload Length: ${channelAdvisorPayload.length}`)
+    log(seperator)
+    log(`Starting Sku Vault Import of ${skuVaultPayload.length} Items`)
     let uploadFunc;
     if(length > 1) {
         log("Bulk Import")
@@ -58,10 +72,11 @@ fastify.post('/import',incomingPayloadSchema, async (request,reply) => {
     // so we don't upload.js a bad sku to Channel Advisor
     let filteredChannelAdvisorPayload = channelAdvisorPayload.filter(({Sku})=>!badSkus.some(({Sku:badSku})=>badSku.includes(Sku)))
     if(filteredChannelAdvisorPayload.length === 0) return reply.send({badSkus});
-    log(`Starting Channel Advisor Import of ${filteredChannelAdvisorPayload.length} Items`)
-
+    log(seperator)
     const access_token = await authorizeChannelAdvisor(tokens);
     log(`Finished Authorizing Channel Advisor`)
+    log(seperator)
+    log(`Starting Channel Advisor Import of ${filteredChannelAdvisorPayload.length} Items`)
     let results = await channelAdvisorImport(
         filteredChannelAdvisorPayload,
         access_token,
@@ -69,8 +84,10 @@ fastify.post('/import',incomingPayloadSchema, async (request,reply) => {
         completedItems
         )
     log(`Finished Channel Advisor Import`)
+    log(seperator)
     results = results.concat(completedItems);
-    log(`Finished Importing ${results.length} Items`)
+    log(`The importer has finished importing ${results.length} items`)
+    log(seperator)
     reply.send({badSkus,
         results
     });
@@ -81,5 +98,7 @@ fastify.listen({port: 3005, host:"10.100.100.51"},(err,addr)=>{
         error(err)
         process.exit(1)
     }
+    log(seperator)
     log(`Server listening at ${addr}`)
+    log(seperator)
 })
