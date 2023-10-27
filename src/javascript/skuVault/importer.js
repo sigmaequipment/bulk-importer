62e00dbb37e002779217fc4c54e3e06e718f0186
mwalkersigma <131814372+mwalkersigma@users.noreply.github.com>
2023-10-27 11:52:23 -0500

    updating logic,

--- src/javascript/skuVault/importer.js
+++ src/javascript/skuVault/importer.js
@@ -2,26 +2,25 @@ const { log, error:err } = require("../Logger/logger");
 const createPromisePool = require("../promisePool/promisePool");
 module.exports = function SkuVaultImporter(uploadFunction){
     return async (payload,token,badSkus) =>{
-    try{
-        const pool = createPromisePool((item)=>uploadFunction(item,token), 5);
-        let skuVaultResults = await pool.process(payload)
-        let responses = await Promise.all(skuVaultResults.results.map(result=>result.json()));
-        log("Finished Uploading To Sku Vault")
-        log("The Responses From Sku Vault Are:",responses.map(({Status})=>Status))
-        log("Checking For Errors")
-        responses.forEach(({Status,Errors},i)=>{
-            Errors.forEach((error)=>{
-                error = uploadFunction.name.toLowerCase().includes("single") ? {Sku:payload[i].Sku,ErrorMessages:[error]} : {...error}
-                err(error)
-                badSkus.push({...error,FailedAt:uploadFunction.name})
+        try{
+            const pool = createPromisePool((item)=>uploadFunction(item,token), 5);
+            let skuVaultResults = await pool.process(payload)
+            let responses = await Promise.all(skuVaultResults.results.map(result=>result.json()));
+            log("Finished Uploading To Sku Vault")
+            log("The Responses From Sku Vault Are:",responses.map(({Status})=>Status))
+            log("Checking For Errors")
+            responses.forEach(({Status,Errors},i)=>{
+                Errors.forEach((error)=>{
+                    error = uploadFunction.name.toLowerCase().includes("single") ? {Sku:payload[i].Sku,ErrorMessages:[error]} : {...error}
+                    err(error)
+                    badSkus.push({...error,FailedAt:uploadFunction.name})
+                })
             })
-        })
-        return responses
-    }catch(e){
-        err("Error Uploading To Sku Vault")
-        err("The Error Is:",e)
-        console.log(e)
-    }
-
+            return responses
+        }catch(e){
+            err("Error Uploading To Sku Vault")
+            err("The Error Is:",e)
+            console.log(e)
+        }
     }
 }
